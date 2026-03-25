f4766e2a55c77967f2d7e96aad88e7daee3b6613
playroomProjects <173646322+cornerstoneMedia@users.noreply.github.com>
2026-03-25 15:11:39 -0400

    Merge progress with develop branch

--- main.py
+++ main.py
@@ -1,27 +1,60 @@
-import matplatlib.pyplot as plt
+import random
+import matplotlib.pyplot as plt
 
-trainingExamples = [["Sunny", "Warm", "Normal", "Strong", "Warm", "Same", True],
-    ["Sunny", "Warm", "High", "Strong", "Warm", "Same", True],
-    ["Rain", "Cold", "High", "Strong", "Warm", "Change", False],
-    ["Sunny", "Warm", "High", "Strong", "Cool", "Change", True]
-]
+
+def generate_rand_concept() -> list[str]:
+    """
+    The generate_rand_concept() function generates a random target concept from <Sunny, Warm, ?, ?, ?, ?>.
+
+    :return: List of random target concepts
+    """
+
+    # :::NOTE:::
+    # if we don't need any neg example since find s ignores them, just remove 'sky' and 'AirTemp' form the dic
+    attr_dic = {
+        "Sky" : ["Sunny", "Cloudy", "Rain"],
+        "AirTemp" : ["Warm", "Cold"],
+        "Humidity" : ["Normal", "High"],
+        "Wind" : ["Strong", "Weak"],
+        "Water" : ["Warm", "Cool"],
+        "Forcast" : ["Same", "Change"]
+    }
+
+    # pick a rand attr-val from the dic
+    target_concept = []
+    for x in attr_dic:
+        target_concept.append(random.choice(attr_dic[x]))
+
+    # if sunny and warm, it must be a true example
+    if target_concept[0] == "Sunny" and target_concept[1] == "Warm":
+        target_concept.append(True)
+    else:
+        target_concept.append(False)
+
+    return target_concept
 
 # Find-S Algorithm Implementation
 # '-' represents the empty set symbol (no value of that attribute is allowed)
-maxSpecific = ['-', '-', '-', '-', '-', '-']
-for instance in trainingExamples:
-    if(instance[6] == True):
-        for i in range(6):
-            if(maxSpecific[i] == '-' or (instance[i] != maxSpecific[i] and maxSpecific[i] != '?')):
-                if(maxSpecific[i] == '-'):
-                    maxSpecific[i] = instance[i]
-                else:
-                    maxSpecific[i] = '?'
-print(maxSpecific)
-
-
+def findS(trainingExamples):
+    maxSpecific = ['-', '-', '-', '-', '-', '-']
+    for instance in trainingExamples:
+        if(instance[6] == True):
+            for i in range(6):
+                if(maxSpecific[i] == '-' or (instance[i] != maxSpecific[i] and maxSpecific[i] != '?')):
+                    if(maxSpecific[i] == '-'):
+                        maxSpecific[i] = instance[i]
+                    else:
+                        maxSpecific[i] = '?'
+    return maxSpecific
 
 def main():
+    """
+
+    :return:
+    """
+
+    rand_target_concept = generate_rand_concept()
+    #print(rand_target_concept)
     pass
 
 if __name__ == "__main__":
