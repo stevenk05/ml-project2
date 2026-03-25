e73cf4bc61b9450f7a3ddb766ebc6395a9dd8176
playroomProjects <173646322+cornerstoneMedia@users.noreply.github.com>
2026-03-25 14:43:33 -0400

    Started implementing Find-S with training example

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 import matplatlib.pyplot as plt
 
+trainingExamples = [["Sunny", "Warm", "Normal", "Strong", "Warm", "Same", True],
+    ["Sunny", "Warm", "High", "Strong", "Warm", "Same", True],
+    ["Rain", "Cold", "High", "Strong", "Warm", "Change", False],
+    ["Sunny", "Warm", "High", "Strong", "Cool", "Change", True]
+]
+
+# Find-S Algorithm Implementation
+# '-' represents the empty set symbol (no value of that attribute is allowed)
+maxSpecific = ['-', '-', '-', '-', '-', '-']
+for instance in trainingExamples:
+    if(instance[6] == True):
+        for i in range(6):
+            if(maxSpecific[i] == '-' or (instance[i] != maxSpecific[i] and maxSpecific[i] != '?')):
+                if(maxSpecific[i] == '-'):
+                    maxSpecific[i] = instance[i]
+                else:
+                    maxSpecific[i] = '?'
+print(maxSpecific)
+
+
 
 def main():
     pass
