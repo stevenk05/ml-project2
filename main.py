19a54b70e30800577eb712a718632ed85434b634
Elvis-Not-Presley-One <lawnguy1201@gmail.com>
2026-03-25 14:57:17 -0400

    feat(): added generate_rand_concepts() function

--- main.py
+++ main.py
@@ -1,7 +1,47 @@
+import random
 import matplotlib.pyplot as plt
 
 
+def generate_rand_concept() -> list[str]:
+    """
+    The generate_rand_concept() function generates a random target concept from <Sunny, Warm, ?, ?, ?, ?>.
+
+    :return: List of random target concepts
+    """
+
+    # :::NOTE:::
+    # if we don't need any neg example since find s ignores them, just remove 'sky' and 'AirTemp' form the dic
+    attr_dic = {
+        "Sky" : ["Sunny", "Cloudy", "Rain"],
+        "AirTemp" : ["Warm", "Cold"],
+        "Humidity" : ["Normal", "High"],
+        "Wind" : ["Strong", "Weak"],
+        "Water" : ["Warm", "Cool"],
+        "Forcast" : ["Same", "Change"]
+    }
+
+    # pick a rand attr-val from the dic
+    target_concept = []
+    for x in attr_dic:
+        target_concept.append(random.choice(attr_dic[x]))
+
+    # if sunny and warm, it must be a true example
+    if target_concept[0] == "Sunny" and target_concept[1] == "Warm":
+        target_concept.append(True)
+    else:
+        target_concept.append(False)
+
+    return target_concept
+
+
 def main():
+    """
+
+    :return:
+    """
+
+    rand_target_concept = generate_rand_concept()
+    #print(rand_target_concept)
     pass
 
 if __name__ == "__main__":
