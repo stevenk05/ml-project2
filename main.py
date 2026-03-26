22325ba058fa3c483c4e68b25f716ae58941fa33
brianmill <brianmill@comcast.net>
2026-03-26 15:34:52 -0400

    D3

--- main.py
+++ main.py
@@ -8,13 +8,17 @@ trainingExamples = [["Sunny", "Warm", "Normal", "Strong", "Warm", "Same", True],
     ["Sunny", "Warm", "High", "Strong", "Cool", "Change", True]
 ]
 
-maxSpecific = ['-', '-', '-', '-', '-', '-']
+MAXSPECIFIC = ['-', '-', '-', '-', '-', '-']
 
-def generate_rand_concept() -> list[str]:
+target_concept = ["Sunny", "Warm", '?', '?', '?', '?']
+
+def generate_rand_training_example() -> list[str]:
     """
-    The generate_rand_concept() function generates a random target concept from <Sunny, Warm, ?, ?, ?, ?>.
+    The generate_rand_training_example() function generates a random training data from <Sunny, Warm, ?, ?, ?, ?>. as the target
+    concept.
 
-    :return: List of random target concepts
+    :return: A single traning example with 6 attributes and a boolean value 
+            indicating whether the example is a positive or negative example of the target concept
     """
 
     # :::NOTE:::
@@ -29,27 +33,26 @@ def generate_rand_concept() -> list[str]:
     }
 
     # pick a rand attr-val from the dic
-    target_concept = []
+    training_example = []
     for x in attr_dic:
-        target_concept.append(random.choice(attr_dic[x]))
+        training_example.append(random.choice(attr_dic[x]))
 
     # if sunny and warm, it must be a true example
-    if target_concept[0] == "Sunny" and target_concept[1] == "Warm":
-        target_concept.append(True)
+    if training_example[0] == "Sunny" and training_example[1] == "Warm":
+        training_example.append(True)
     else:
-        target_concept.append(False)
+        training_example.append(False)
 
-    return target_concept
+    return training_example
 
 # Find-S Algorithm Implementation
 # '-' represents the empty set symbol (no value of that attribute is allowed)
-def findS(trainingExamples, temp_hypothesis = maxSpecific):
+def findS(trainingExamples, temp_hypothesis = MAXSPECIFIC):
     for instance in trainingExamples:
         if(instance[6] == True):
             for i in range(6):
                 if(temp_hypothesis[i] == '-' or (instance[i] != temp_hypothesis[i] and temp_hypothesis[i] != '?')):
                     if(temp_hypothesis[i] == '-'):
-                        print("RAN")
                         temp_hypothesis[i] = instance[i]
                     else:
                         temp_hypothesis[i] = '?'
@@ -57,18 +60,16 @@ def findS(trainingExamples, temp_hypothesis = maxSpecific):
 
 def count_iterations():
 
-    temp_hypothesis = maxSpecific
+    temp_hypothesis = MAXSPECIFIC.copy()
     iterations = 0
     random_data = []
-    random_data.append(generate_rand_concept())
+    random_data.append(generate_rand_training_example())
 
-    while '?' or '-' in temp_hypothesis:
-        random_data = []
-        random_data.append(generate_rand_concept())
-        temp_hypothesis = findS(random_data, temp_hypothesis)
+    while temp_hypothesis != target_concept:
+        training_example = []
+        training_example.append(generate_rand_training_example())
+        temp_hypothesis = findS(training_example, temp_hypothesis)
         iterations += 1
-        print(temp_hypothesis)
-        sleep(1)
 
     return iterations
 
@@ -79,15 +80,16 @@ def main():
     :return:
     """
 
+    experiment_iterations = 100
+    iteration_counts = []
+
     #hypthesis = findS(trainingExamples)
     #print("The maximally specific hypothesis is: ", hypthesis)
 
-    print(count_iterations())
+    for _ in range(experiment_iterations):
+        iteration_counts.append(count_iterations())
 
-    # for i in range(100):
-    #     rand_target_concept = generate_rand_concept()
-    #     print(len(rand_target_concept))
-
+    print(iteration_counts)
 
 if __name__ == "__main__":
     main()
